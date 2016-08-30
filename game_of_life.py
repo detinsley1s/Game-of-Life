5a95e68c4c955d7cd7421f591eb35fe58cebd20b
Daniel Tinsley <detinsley1s@gmail.com>
2016-08-30 16:57:22 -0500

    The game now performs the living/dying algorithm correctly.

--- game_of_life.py
+++ game_of_life.py
@@ -7,6 +7,7 @@ Copyright 2016
 The classic life simulation.
 """
 
+from random import randint
 import sge
 
 
@@ -23,6 +24,12 @@ class Game(sge.dsp.Game):
     def event_key_press(self, key, char):
         if key == 'escape':
             self.event_close()
+        elif key == 'r':
+            grid.randomize()
+        elif key == 's':
+            grid.grid_is_alive = not grid.grid_is_alive
+        elif key == 'c':
+            grid.clear()
 
     def event_close(self):
         self.end()
@@ -38,26 +45,30 @@ class Game(sge.dsp.Game):
 class Room(sge.dsp.Room):
     def event_step(self, time_passed, delta_mult):
         for tile in grid.grid:
-            if game_is_running:
-                alive, dead = grid.get_total_neighbors(tile.x, tile.y)
+            if grid.grid_is_alive:
+                alive = grid.get_total_neighbors(tile.x, tile.y)
                 col = tile.x // TILE_DIMS
                 row = tile.y // TILE_DIMS
                 if tile.is_alive and (
                         alive < 2 or alive > 3) or (
-                        not tile.is_alive and dead == 3):
+                        not tile.is_alive and alive == 3):
                     grid.change_cell(row, col)
+        for idx, tile in enumerate(grid.grid):
             sge.game.project_sprite(tile.sprite, 0, tile.x, tile.y)
+            grid.grid[idx].changed = False
 
 
 class Tile(sge.dsp.Object):
-    def __init__(self, x, y, is_alive=False):
+    def __init__(self, x, y, is_alive=False, changed=False):
         self.is_alive = is_alive
+        self.changed = changed
         sprite = ALIVE_SPRITE if self.is_alive else DEAD_SPRITE
         super().__init__(x, y, sprite=sprite)
 
 
 class Grid:
     def __init__(self):
+        self.grid_is_alive = False
         self.grid = []
         for row in range(GRID_DIMS):
             for col in range(GRID_DIMS):
@@ -71,12 +82,12 @@ class Grid:
         actual_loc = mouse_x_loc*GRID_DIMS + mouse_y_loc
         self.grid[actual_loc] = Tile(
             mouse_y_loc*TILE_DIMS, mouse_x_loc*TILE_DIMS,
-            not self.grid[actual_loc].is_alive
+            not self.grid[actual_loc].is_alive,
+            changed = True
         )
 
     def get_total_neighbors(self, x, y):
-        total_live = 0
-        total_dead = 0
+        total = 0
         col = x // TILE_DIMS
         row = y // TILE_DIMS
         for i in range(row-1, row+2):
@@ -84,12 +95,22 @@ class Grid:
                 if (i, j) != (row, col):
                     i %= GRID_DIMS
                     j %= GRID_DIMS
-                    if self.grid[i*GRID_DIMS + j].is_alive:
-                        total_live += 1
+                    if self.grid[i*GRID_DIMS + j].changed:
+                        if not self.grid[i*GRID_DIMS + j].is_alive:
+                            total += 1
                     else:
-                        total_dead += 1
-        return total_live, total_dead
+                        if self.grid[i*GRID_DIMS + j].is_alive:
+                            total += 1
+        return total
 
+    def randomize(self):
+        for row in range(GRID_DIMS):
+            for col in range(GRID_DIMS):
+                if randint(0, 2) == 1:
+                    self.change_cell(col, row)
+
+    def clear(self):
+        pass
 
 Game(
     width=WINDOW_WIDTH, height=WINDOW_HEIGHT,
@@ -119,8 +140,6 @@ BACKGROUND = sge.gfx.Background([], sge.gfx.Color("blue"))
 sge.game.start_room = Room([], background=BACKGROUND)
 
 sge.game.mouse.visible = True
-
-game_is_running = False
 
 if __name__ == '__main__':
     sge.game.start()
